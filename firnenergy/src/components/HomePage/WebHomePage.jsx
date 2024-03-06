49f34dbbf1a2869ca3de52084aae73d525a54ba4
IlirBerisha06 <162096427+IlirBerisha06@users.noreply.github.com>
2024-03-06 14:37:46 +0100

    taalveranderingen

--- firnenergy/src/components/HomePage/WebHomePage.jsx
+++ firnenergy/src/components/HomePage/WebHomePage.jsx
@@ -78,9 +78,9 @@ const WebHomePage = () => {
                 <div className="HomePage-Navbar">
                     <div className="HomePage-Navbar-Logo"><img className="HomePage-Navbar-Logo" src={FirnLogoHorWhite} alt="FirnLogoWhiteHor" /></div>
                     <div className="HomePage-Navbar-Items">
-                        <div className="HomePage-Navbar-Items-About HomePage-Navbar-Pointer"><a href="#HomePage-AboutUs-id">About</a></div>
-                        <div className="HomePage-Navbar-Items-Plans HomePage-Navbar-Pointer"><a href="#HomePage-Plans-id">Plans</a></div>
-                        <div className="HomePage-Navbar-Items-Contact HomePage-Navbar-Pointer"><a href="#HomePage-Contanct-Id">Contact</a></div>
+                        <div className="HomePage-Navbar-Items-About HomePage-Navbar-Pointer"><a href="#HomePage-AboutUs-id">{t("NavbarOverons")}</a></div>
+                        <div className="HomePage-Navbar-Items-Plans HomePage-Navbar-Pointer"><a href="#HomePage-Plans-id">{t("NavbarPlan")}</a></div>
+                        <div className="HomePage-Navbar-Items-Contact HomePage-Navbar-Pointer"><a href="#HomePage-Contanct-Id">{t("Contact")}</a></div>
                         <div className="HomePage-Navbar-Items-Dashboard HomePage-Navbar-Pointer">
                             <div className="HomePage-Navbar-Items-Dashboard-Button">
                                 <div className="HomePage-Navbar-Items-Dashboard-Text">Dashboard</div>
@@ -98,7 +98,7 @@ const WebHomePage = () => {
                             <div className="HomePage-Overlay-Text-Title"> Firn <span className="HomePage-Overlay-Text-Title-Color">Energy</span></div>
                             <div className="HomePage-Overlay-Text-Desc">{t("HoofdtekstDesc")}</div>
                             <div className="HomePage-Overlay-Text-Button-Container">
-                                <div><button className="HomePage-Overlay-text-Button">Contact us</button></div>
+                                <div><button className="HomePage-Overlay-text-Button">{t("HoofdtekstKnop")}</button></div>
                             </div>
                         </div>
                     </div>
@@ -116,10 +116,10 @@ const WebHomePage = () => {
                         <img className="HomePage-Plans-ShapeOne-img" src={ShapeOne} alt="" />
                         <div className="HomePage-Plans-ShapeOne-Text">
                             <div className="HomePage-Plans-ShapeOne-Text-Title">
-                                <p className="HomePage-Plans-ShapeOne-Text-Title-Text">What is FirnEnergy?</p>
+                                <p className="HomePage-Plans-ShapeOne-Text-Title-Text">{t("TitelMiddenstuk")}</p>
                             </div>
                             <div className="HomePage-Plans-ShapeOne-Text-Desc">
-                                <p>FIRN Energy is a leading company focusing on developing advanced battery solutions and energy management systems. We believe in the power of energy storage and the impact it can have on the way we consume and manage energy. Our mission is to provide sustainable and reliable energy storage solutions that support the transition to a greener future.</p>
+                                <p>{t("DescMiddenstuk")}</p>
                             </div>
                         </div>
                     </div>
@@ -149,46 +149,46 @@ const WebHomePage = () => {
                         <div className="HomePage-Plans-Industrial-Icon">
                             <img width={"60%"} src={IndustiralIcon} alt="" />
                         </div>
-                        <div className="HomePage-Plans-Industrial-Text-Titel">Industrial</div>
-                        <div style={isIndustrialHovered || selectedPlan == "Industrial" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Text-desc">Maximize your company’s energy by controlling your energy-flow.</div>
-                        <div onClick={() => navigate("/Industrie")} style={isIndustrialHovered || selectedPlan == "Industrial" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Button">{"Learn more ->"}</div>
+                        <div className="HomePage-Plans-Industrial-Text-Titel">{t("IndustrieelTitel")}</div>
+                        <div style={isIndustrialHovered || selectedPlan == "Industrial" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Text-desc">{t("IndustrieelDesc")}</div>
+                        <div onClick={() => navigate("/Industrie")} style={isIndustrialHovered || selectedPlan == "Industrial" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Button">{t("IndustrieelKnop")}</div>
                     </div>
                     {/*Residential*/}
                     <div style={isResidentialHovered || selectedPlan == "Residential" ? {background:"#3b2515", transition: "background 0.5s"} : {background:"none", transition: "background 0.5s"}} className={selectedPlan == "Residential" ? "HomePage-Plans-Industrial-Selected" : "HomePage-Plans-Industrial"}  onClick={() => {setSelectedPlan("Residential")}} onMouseEnter={() => {setIsResidentialHovered(true)}} onMouseLeave={() => {setIsResidentialHovered(false)}}>
                         <div className="HomePage-Plans-Industrial-Icon">
                             <img width={"70%"} src={ResidentialIcon} alt="" />
                         </div>
-                        <div className="HomePage-Plans-Industrial-Text-Titel">Residential</div>
-                        <div style={isResidentialHovered || selectedPlan == "Residential" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Text-desc">Optimize your battery setup by importing and exporting at the right time.</div>
-                        <div onClick={() => navigate("/Residentieel")} style={isResidentialHovered || selectedPlan == "Residential" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Button">{"Learn more ->"}</div>
+                        <div className="HomePage-Plans-Industrial-Text-Titel">{t("ResidentieelTitel")}</div>
+                        <div style={isResidentialHovered || selectedPlan == "Residential" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Text-desc">{t("ResidentieelDesc")}</div>
+                        <div onClick={() => navigate("/Residentieel")} style={isResidentialHovered || selectedPlan == "Residential" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Button">{t("ResidentieelKnop")}</div>
                     </div>
                     {/*landbouw*/}
                     <div style={isAgricultureHovered || selectedPlan == "Agriculture" ? {background:"#3b2515", transition: "background 0.5s"} : {background:"none", transition: "background 0.5s"}} className={selectedPlan == "Agriculture" ? "HomePage-Plans-Industrial-Selected" : "HomePage-Plans-Industrial"} onClick={() => {setSelectedPlan("Agriculture")}} onMouseEnter={() => {setIsAgricultureHovered(true)}} onMouseLeave={() => {setIsAgricultureHovered(false)}}>
                         <div className="HomePage-Plans-Industrial-Icon">
                             <img width={"60%"} src={AgricultureIcon} alt="" />
                         </div>
-                        <div className="HomePage-Plans-Industrial-Text-Titel">Agriculture</div>
-                        <div style={isAgricultureHovered || selectedPlan == "Agriculture" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Text-desc">Avoid significant energy costs thanks to VLIF and increase your energy storage.</div>
-                        <div onClick={() => navigate("/Horeca")} style={isAgricultureHovered || selectedPlan == "Agriculture" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Button">{"Learn more ->"}</div>
+                        <div className="HomePage-Plans-Industrial-Text-Titel">{t("LandbouwTitel")}</div>
+                        <div style={isAgricultureHovered || selectedPlan == "Agriculture" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Text-desc">{t("LandbouwDesc")}</div>
+                        <div onClick={() => navigate("/Horeca")} style={isAgricultureHovered || selectedPlan == "Agriculture" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Button">{t("LandbouwKnop")}</div>
                     </div>
                     {/*horeca*/}
                     <div style={isHospitalityHoverd || selectedPlan == "Hospitality" ? {background:"#3b2515", transition: "background 0.5s"} : {background:"none", transition: "background 0.5s"}} className={selectedPlan == "Hospitality" ? "HomePage-Plans-Industrial-Selected" : "HomePage-Plans-Industrial"} onClick={() => {setSelectedPlan("Hospitality")}} onMouseEnter={() => {setIsHospitalityHoverd(true)}} onMouseLeave={() => {setIsHospitalityHoverd(false)}}>
                         <div className="HomePage-Plans-Industrial-Icon">
                             <img style={{marginTop:"-5px"}} width={"60%"} src={HospitalityIcon} alt="" />
                         </div>
-                        <div className="HomePage-Plans-Industrial-Text-Titel">Hospitality</div>
-                        <div style={isHospitalityHoverd || selectedPlan == "Hospitality" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Text-desc">Work with the help of day-ahead hourly prices to efficiently run your hospitality business.</div>
-                        <div onClick={() => navigate("/Landbouw")} style={isHospitalityHoverd || selectedPlan == "Hospitality" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Button">{"Learn more ->"}</div>
+                        <div className="HomePage-Plans-Industrial-Text-Titel">{t("GastvrijheidTitel")}</div>
+                        <div style={isHospitalityHoverd || selectedPlan == "Hospitality" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Text-desc">{t("GastvrijheidDesc")}</div>
+                        <div onClick={() => navigate("/Landbouw")} style={isHospitalityHoverd || selectedPlan == "Hospitality" ? {} : {visibility:"hidden"}} className="HomePage-Plans-Industrial-Button">{t("GastvrijheidKnop")}</div>
                     </div>
                     {/*Keypoints*/}
                     <div className="HomePage-Plans-Keypoints">
-                        <div className="HomePage-Plans-Keypoints-Title">Plan Keypoints</div>
+                        <div className="HomePage-Plans-Keypoints-Title">{t("LijstTitel")}</div>
                         <div className="HomePage-Plans-Keypoints-Text-Container">
-                            <div className="HomePage-Plans-Keypoints-Text"><div className="HomePage-Plans-Keypoints-bullet"></div> <div>{KeypointsPerPlan[selectedPlan][0]}</div></div>
-                            <div className="HomePage-Plans-Keypoints-Text"><div className="HomePage-Plans-Keypoints-bullet"></div> <div>{KeypointsPerPlan[selectedPlan][1]}</div></div>
-                            <div className="HomePage-Plans-Keypoints-Text"><div className="HomePage-Plans-Keypoints-bullet"></div> <div>{KeypointsPerPlan[selectedPlan][2]}</div></div>
+                            <div className="HomePage-Plans-Keypoints-Text"><div className="HomePage-Plans-Keypoints-bullet"></div> <div>{t("LijstGegeven1")}</div></div>
+                            <div className="HomePage-Plans-Keypoints-Text"><div className="HomePage-Plans-Keypoints-bullet"></div> <div>{t("LijstGegeven2")}</div></div>
+                            <div className="HomePage-Plans-Keypoints-Text"><div className="HomePage-Plans-Keypoints-bullet"></div> <div>{t("LijstGegeven3")}</div></div>
                         </div>
-                        <div><button onClick={navigateToProfile} className="HomePage-Plans-Keypoints-Button">View details</button></div>
+                        <div><button onClick={navigateToProfile} className="HomePage-Plans-Keypoints-Button">{t("LijstKnop")}</button></div>
                     </div>
                 </div>
                 
@@ -200,8 +200,8 @@ const WebHomePage = () => {
                 <div className="HomePage-DayAhead-Text">
                     <img width={"100%"} height={"110%"} src={ShapeFour} alt="" />
                     <div className="HomePage-DayAhead-Text-Container">
-                        <div className="HomePage-DayAhead-Text-Title">Price based decisions</div>
-                        <div className="HomePage-DayAhead-Text-Desc">The Firncontroller makes decisions based on real-time price data by utilizing the day-ahead price. Therefor it knows when it is best to sell or to buy energy, so that you get the most out of your solar-installation without having to micromanage everything.</div>
+                        <div className="HomePage-DayAhead-Text-Title">{t("OnderstukTitel")}</div>
+                        <div className="HomePage-DayAhead-Text-Desc">{t("OnderstukDesc")}</div>
                     </div>
                 </div>
             </div>
